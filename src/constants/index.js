719d1c34fa354025bd0c1ca27c6b18d24ac61b8b
Chu Phong <man.nguyen.cit@gmail.com>
2023-10-06 01:31:35 +0700

    update productDetail

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+export const LOCATIONS = {
+    // Admin
+    // ADMIN: '/admin',
+    // AD_CATEGORY:'/admin/categories',
+    // AD_SUPPLIER:'/admin/suppliers',
+    // AD_PRODUCTS:'/admin/products',
+    // AD_CUSTOMER:'/admin/customers',
+    //User
+    HOME_PAGE: '/',
+    ABOUT_PAGE: '/about',
+    // LOGIN: '/login',
+    PRODUCTS: '/products',
+    PRODUCT_DETAILS: '/products/:id',
+}
